36e7df5e7e7ea5e90597e0af72dce91cc13caf84
Dinh Le <dinh.v.le@gmail.com>
2016-07-15 15:57:16 -0500

    nav-bar, and main form is done

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 (function() {
   var app = angular.module('redditish', ['ngAnimate', 'ui.bootstrap']);
 
+  app.controller('PostController', function() {
+    this.posts = pokemons;
+  })
+
   app.controller('DropdownCtrl', function($scope, $log) {
     $scope.items = [
       'Votes',
@@ -24,4 +28,33 @@
 
     $scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
   });
+
+  var pokemons = [{
+      title: "Ivysaur",
+      author: "Ash",
+      imageurl: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png" ,
+      description: "There is a bud on this Pokémon's back. To support its weight, Ivysaur's legs and trunk grow thick and strong. If it starts spending more time lying in the sunlight, it's a sign that the bud will bloom into a large flower soon.",
+      comments: [{
+        author: "TeamValor",
+        text: "We only like red pokemons!"
+      }, {
+        author: "TeamCool",
+        text: "We love it!"
+      }],
+      createOn: 1397490980837
+    }, {
+      title: "Metapod",
+      author: "Rock",
+      imageurl: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/011.png" ,
+      description: "The shell covering this Pokémon's body is as hard as an iron slab. Metapod does not move very much. It stays still because it is preparing its soft innards for evolution inside the hard shell.",
+      comments: [{
+        author: "TeamValor",
+        text: "Eww it's a bug!"
+      }, {
+        author: "TeamCool",
+        text: "We like it!"
+      }],
+      createOn: 1397490980837
+    }];
+
 })();
